1a45796331ca461fd5d68ef0f94662d25bc9fc14
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2023-10-09 23:45:59 +0530

    fix:ghpages build issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   publicRuntimeConfig: {
     NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
   },
